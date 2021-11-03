e48e3bb2a15c79710bf94d00fb8b4b218e01bf1d
Spencer Hamilton <shamilton05@berkeley.edu>
2021-11-02 18:54:39 -0700

    added experience

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import { App } from './App'
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <a style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>
-        software
-        <br />
-        digital art
+      <a href="/resume.pdf" target="_blank" class="fcc-btn" style={{ position: 'absolute', bottom: 40, left: 40, fontSize: '13px' }}>
+        experience
       </a>
       <a href="https://www.linkedin.com/in/spencer-h/" target="_blank" style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>
         spencer hamilton
